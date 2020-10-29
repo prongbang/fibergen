4d27c201b92fe97a709af293165bbfeba298e149
anonymous <anonymous>
2020-10-29 22:43:50 +0700

    initial

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/prongbang/fibergen/pkg/filex"
 	"github.com/prongbang/fibergen/pkg/genx"
 )
 
 func main() {
-	feature := flag.String("f", "", "-f=feature-name")
+	feature := flag.String("f", "", "-f feature-name")
 	flag.Parse()
+
+	if *feature == "" {
+		fmt.Println("Please use: fibergen -f feature-name")
+		return
+	}
 
 	fx := filex.NewFileX()
 	gen := genx.NewGenerator(fx)
