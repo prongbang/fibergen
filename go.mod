2401efd1762ccdfd1029e88e3e7162dda172611e
prongbang <prongbang@gmail.com>
2023-12-30 00:58:31 +0700

    refactor: flag core to cli

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/prongbang/fibergen
 
 go 1.15
 
-require github.com/pterm/pterm v0.12.59
+require (
+	github.com/pterm/pterm v0.12.59
+	github.com/urfave/cli/v2 v2.27.0
+)
--- main.go
+++ main.go
@@ -1,40 +1,90 @@
 package main
 
 import (
-	"flag"
+	"errors"
 	"fmt"
+	"log"
+	"os"
 	"strings"
-
-	"github.com/prongbang/fibergen/pkg/option"
+	"time"
 
 	"github.com/prongbang/fibergen/pkg/filex"
 	"github.com/prongbang/fibergen/pkg/genx"
+	"github.com/prongbang/fibergen/pkg/option"
+	"github.com/urfave/cli/v2"
 )
 
+type Flags struct {
+	N string
+	M string
+	F string
+}
+
+func (f Flags) Project() string {
+	return strings.ReplaceAll(strings.ReplaceAll(f.N, " ", "_"), "-", "_")
+}
+
+func (f Flags) Module() string {
+	return fmt.Sprintf("%s/%s", f.M, f.Project())
+}
+
+func (f Flags) Feature() string {
+	return strings.ReplaceAll(strings.ReplaceAll(f.F, " ", "_"), "-", "_")
+}
+
 func main() {
-	projectName := flag.String("new", "", "-new project-name")
-	modName := flag.String("mod", "", "-mod module-name")
-	featureName := flag.String("f", "", "-f featureName-name")
-	flag.Parse()
+	flags := Flags{}
 
-	if *featureName == "" && *projectName == "" && *modName == "" {
-		fmt.Println("Please use: fibergen -f featureName-name")
-		return
+	app := &cli.App{
+		Name:      "fibergen",
+		Usage:     "Generate a Clean Architecture for REST API with support for the Fiber Web Framework in Golang",
+		Version:   "v1.0.6",
+		Compiled:  time.Now(),
+		Copyright: "(c) 2023 prongbang",
+		Authors: []*cli.Author{
+			{
+				Name:  "prongbang",
+				Email: "github.com/prongbang",
+			},
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "new",
+				Aliases:     []string{"n"},
+				Usage:       "-n project-name",
+				Destination: &flags.N,
+			},
+			&cli.StringFlag{
+				Name:        "mod",
+				Aliases:     []string{"m"},
+				Usage:       "-m github.com/prongbang/module-name",
+				Destination: &flags.M,
+			},
+			&cli.StringFlag{
+				Name:        "feature",
+				Aliases:     []string{"f"},
+				Usage:       "-f auth",
+				Destination: &flags.F,
+			},
+		},
+		Action: func(*cli.Context) error {
+			if flags.F == "" && flags.N == "" && flags.M == "" {
+				return errors.New("please use: fibergen -f feature-name")
+			}
+
+			if flags.N == "" && flags.M == "" && flags.F == "" {
+				return errors.New("please use: fibergen -n project-name -m github.com/prongbang/module-name")
+			}
+
+			opt := option.Options{Project: flags.Project(), Module: flags.Module(), Feature: flags.Feature()}
+			gen := genx.NewGenerator(filex.NewFileX())
+			gen.GenerateAll(opt)
+
+			return nil
+		},
 	}
 
-	if *projectName == "" && *modName == "" && *featureName == "" {
-		fmt.Println("Please use: fibergen -new project-name -mod github.com/prongbang/name")
-		return
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
-
-	project := strings.ReplaceAll(strings.ReplaceAll(*projectName, " ", "_"), "-", "_")
-	opt := option.Options{
-		Project: project,
-		Module:  fmt.Sprintf("%s/%s", *modName, project),
-		Feature: *featureName,
-	}
-
-	fx := filex.NewFileX()
-	gen := genx.NewGenerator(fx)
-	gen.GenerateAll(opt)
 }
