a5f4bd05357e28a4ccef7dfeba8886a17851b92b
prongbang <prongbang@gmail.com>
2024-07-01 20:41:16 +0700

    feat: support curd postgresql

---
+++ pkg/template/datasource_crud_postgres_template.go
@@ -0,0 +1,156 @@
+package template
+
+import (
+	"strings"
+
+	"github.com/ettle/strcase"
+)
+
+func DataSourceCrudPostgrest(
+	model string,
+	module string,
+	path string,
+	pk string,
+	driverName string,
+	queryColumns string,
+	insertValues string,
+	insertFields string,
+	insertQuestions string,
+	updateSets string,
+) string {
+	tmpl := `package {name}
+	
+	import (
+		"fmt"
+		"{module}/{path}/database"
+		"github.com/prongbang/sqlxwrapper/pqwrapper"
+		"github.com/prongbang/goerror"
+	)
+	
+	type DataSource interface {
+		Count(params Params) int64
+		FindList(params Params) []{model}
+		FindById(id {pk}) {model}
+		Create(data *Create{model}) error
+		Update(data *Update{model}) error
+		Delete(id {pk}) error
+	}
+	
+	type dataSource struct {
+		Driver database.Drivers
+	}
+	
+	func (d *dataSource) Count(params Params) int64 {
+		conn := d.Driver.{driver}()
+		sql := "SELECT COUNT(id) FROM {table} WHERE 1=1 %s"
+		wheres := ""
+		args := []any{}
+	
+		sql = fmt.Sprintf(sql, wheres)
+	
+		return pqwrapper.Count(conn, sql, args...)
+	}
+	
+	func (d *dataSource) FindList(params Params) []{model} {
+		conn := d.Driver.{driver}()
+		sql := "SELECT {columns} FROM {table} {alias} WHERE 1=1 %s ORDER BY {alias}.id"
+		wheres := ""
+		args := []any{}
+	
+		if params.Limit > 0 && params.Offset >= 0 {
+			sql += " LIMIT ?"
+			args = append(args, params.Limit)
+			sql += " OFFSET ?"
+			args = append(args, params.Offset)
+		}
+	
+		sql = fmt.Sprintf(sql, wheres)
+	
+		return pqwrapper.SelectList[{model}](conn, sql, args...)
+	}
+	
+	func (d *dataSource) FindById(id {pk}) {model} {
+		conn := d.Driver.{driver}()
+		sql := "SELECT {columns} FROM {table} {alias} WHERE {alias}.id = ?"
+	
+		return pqwrapper.SelectOne[{model}](conn, sql, id)
+	}
+	
+	func (d *dataSource) Create(data *Create{model}) error {
+		conn := d.Driver.{driver}()
+		sql := "INSERT INTO {table} ({fields}) VALUES ({questions})"
+		args := []any{
+			{values}
+		}
+		tx, err := pqwrapper.Create(conn, sql, []any{&data.Id}, args...)
+		if err == nil {
+			if e := tx.Commit(); e != nil {
+				return goerror.NewError(goerror.Body{Code: "DTB001", Message: "Cannot add a child row"})
+			}
+			return nil
+		}
+		return err
+	}
+	
+	func (d *dataSource) Update(data *Update{model}) error {
+		conn := d.Driver.{driver}()
+		params := map[string]interface{}{
+			"id": data.Id,
+		}
+		set := ""
+		sql := "UPDATE {table} SET %s WHERE id=:id"
+	
+		{sets}
+	
+		tx, err := pqwrapper.Update(conn, sql, set, params)
+		if err == nil {
+			if e := tx.Commit(); e != nil {
+				return goerror.NewError(goerror.Body{Code: "DTB001", Message: "Cannot add a child row"})
+			}
+			return nil
+		}
+		return err
+	}
+	
+	func (d *dataSource) Delete(id {pk}) error {
+		conn := d.Driver.{driver}()
+		sql := "DELETE FROM {table} WHERE id=?"
+	
+		tx, err := pqwrapper.Delete(conn, sql, id)
+		if err == nil {
+			if e := tx.Commit(); e != nil {
+				return goerror.NewError(goerror.Body{Code: "DTB001", Message: "Cannot add a child row"})
+			}
+			return nil
+		}
+		return err
+	}
+	
+	func NewDataSource(driver database.Drivers) DataSource {
+		return &dataSource{
+			Driver: driver,
+		}
+	}`
+
+	tmpl = strings.ReplaceAll(tmpl, "{module}", module)
+	tmpl = strings.ReplaceAll(tmpl, "{path}", path)
+	tmpl = strings.ReplaceAll(tmpl, "{pk}", pk)
+	tmpl = strings.ReplaceAll(tmpl, "{model}", strcase.ToPascal(model))
+	tmpl = strings.ReplaceAll(tmpl, "{driver}", driverName)
+	tmpl = strings.ReplaceAll(tmpl, "{name}", strings.ToLower(model))
+
+	// Query
+	tmpl = strings.ReplaceAll(tmpl, "{table}", strings.ToLower(model))
+	tmpl = strings.ReplaceAll(tmpl, "{alias}", strings.ToLower(model)[:1])
+	tmpl = strings.ReplaceAll(tmpl, "{columns}", queryColumns)
+
+	// Insert
+	tmpl = strings.ReplaceAll(tmpl, "{values}", insertValues)
+	tmpl = strings.ReplaceAll(tmpl, "{fields}", insertFields)
+	tmpl = strings.ReplaceAll(tmpl, "{questions}", insertQuestions)
+
+	// Update
+	tmpl = strings.ReplaceAll(tmpl, "{sets}", updateSets)
+
+	return tmpl
+}
--- pkg/template/datasource_crud_template.go
+++ pkg/template/datasource_crud_template.go
@@ -1,10 +1,7 @@
 package template
 
 import (
-	"log"
-	"strings"
-
-	"github.com/ettle/strcase"
+	"fmt"
 )
 
 func DataSourceCrud(
@@ -19,148 +16,32 @@ func DataSourceCrud(
 	insertQuestions string,
 	updateSets string,
 ) string {
-	driverName := ""
 	if driver == "mariadb" {
-		driverName = "GetMariaDB"
+		return DataSourceCrudMariaDb(
+			model,
+			module,
+			path,
+			pk,
+			"GetMariaDB",
+			queryColumns,
+			insertValues,
+			insertFields,
+			insertQuestions,
+			updateSets,
+		)
 	} else if driver == "postgres" {
-		driverName = "GetPostgresDB"
-	} else {
-		log.Fatal("unsupported driver", driver)
+		return DataSourceCrudPostgrest(
+			model,
+			module,
+			path,
+			pk,
+			"GetPostgresDB",
+			queryColumns,
+			insertValues,
+			insertFields,
+			insertQuestions,
+			updateSets,
+		)
 	}
-
-	tmpl := `package {name}
-	
-	import (
-		"fmt"
-		"{module}/{path}/database"
-		"github.com/prongbang/sqlxwrapper/mrwrapper"
-		"github.com/prongbang/goerror"
-	)
-	
-	type DataSource interface {
-		Count(params Params) int64
-		FindList(params Params) []{model}
-		FindById(id {pk}) {model}
-		Create(data *Create{model}) error
-		Update(data *Update{model}) error
-		Delete(id {pk}) error
-	}
-	
-	type dataSource struct {
-		Driver database.Drivers
-	}
-	
-	func (d *dataSource) Count(params Params) int64 {
-		conn := d.Driver.{driver}()
-		sql := "SELECT COUNT(id) FROM {table} WHERE 1=1 %s"
-		wheres := ""
-		args := []any{}
-	
-		sql = fmt.Sprintf(sql, wheres)
-	
-		return mrwrapper.Count(conn, sql, args...)
-	}
-	
-	func (d *dataSource) FindList(params Params) []{model} {
-		conn := d.Driver.{driver}()
-		sql := "SELECT {columns} FROM {table} {alias} WHERE 1=1 %s ORDER BY {alias}.id"
-		wheres := ""
-		args := []any{}
-	
-		if params.Limit > 0 && params.Offset >= 0 {
-			sql += " LIMIT ?"
-			args = append(args, params.Limit)
-			sql += " OFFSET ?"
-			args = append(args, params.Offset)
-		}
-	
-		sql = fmt.Sprintf(sql, wheres)
-	
-		return mrwrapper.SelectList[{model}](conn, sql, args...)
-	}
-	
-	func (d *dataSource) FindById(id {pk}) {model} {
-		conn := d.Driver.{driver}()
-		sql := "SELECT {columns} FROM {table} {alias} WHERE {alias}.id = ?"
-	
-		return mrwrapper.SelectOne[{model}](conn, sql, id)
-	}
-	
-	func (d *dataSource) Create(data *Create{model}) error {
-		conn := d.Driver.GetMariaDB()
-		sql := "INSERT INTO {table} ({fields}) VALUES ({questions})"
-		args := []any{
-			{values}
-		}
-		tx, err := mrwrapper.Create(conn, sql, []any{&data.Id}, args...)
-		if err == nil {
-			if e := tx.Commit(); e != nil {
-				return goerror.NewError(goerror.Body{Code: "DTB001", Message: "Cannot add a child row"})
-			}
-			return nil
-		}
-		return err
-	}
-	
-	func (d *dataSource) Update(data *Update{model}) error {
-		conn := d.Driver.{driver}()
-		params := map[string]interface{}{
-			"id": data.Id,
-		}
-		set := ""
-		sql := "UPDATE {table} SET %s WHERE id=:id"
-	
-		{sets}
-	
-		tx, err := mrwrapper.Update(conn, sql, set, params)
-		if err == nil {
-			if e := tx.Commit(); e != nil {
-				return goerror.NewError(goerror.Body{Code: "DTB001", Message: "Cannot add a child row"})
-			}
-			return nil
-		}
-		return err
-	}
-	
-	func (d *dataSource) Delete(id {pk}) error {
-		conn := d.Driver.{driver}()
-		sql := "DELETE FROM {table} WHERE id=?"
-	
-		tx, err := mrwrapper.Delete(conn, sql, id)
-		if err == nil {
-			if e := tx.Commit(); e != nil {
-				return goerror.NewError(goerror.Body{Code: "DTB001", Message: "Cannot add a child row"})
-			}
-			return nil
-		}
-		return err
-	}
-	
-	func NewDataSource(driver database.Drivers) DataSource {
-		return &dataSource{
-			Driver: driver,
-		}
-	}`
-
-	tmpl = strings.ReplaceAll(tmpl, "{module}", module)
-	tmpl = strings.ReplaceAll(tmpl, "{path}", path)
-	tmpl = strings.ReplaceAll(tmpl, "{pk}", pk)
-	tmpl = strings.ReplaceAll(tmpl, "{model}", strcase.ToPascal(model))
-	tmpl = strings.ReplaceAll(tmpl, "{driver}", driverName)
-	tmpl = strings.ReplaceAll(tmpl, "{name}", strings.ToLower(model))
-
-	// Query
-	tmpl = strings.ReplaceAll(tmpl, "{table}", strings.ToLower(model))
-	tmpl = strings.ReplaceAll(tmpl, "{alias}", strings.ToLower(model)[:1])
-	tmpl = strings.ReplaceAll(tmpl, "{columns}", queryColumns)
-
-	// Insert
-	tmpl = strings.ReplaceAll(tmpl, "{values}", insertValues)
-	tmpl = strings.ReplaceAll(tmpl, "{fields}", insertFields)
-	tmpl = strings.ReplaceAll(tmpl, "{questions}", insertQuestions)
-
-	// Update
-	tmpl = strings.ReplaceAll(tmpl, "{sets}", updateSets)
-
-	return tmpl
+	panic(fmt.Sprintf("unsupported driver: %s", driver))
 }
