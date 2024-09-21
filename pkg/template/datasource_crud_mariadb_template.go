82e6c3a863d0f3e5c04f660a7f5dd40553eff1ad
prongbang <prongbang@gmail.com>
2024-09-21 14:14:46 +0700

    chore: reformat

--- pkg/template/datasource_crud_mariadb_template.go
+++ pkg/template/datasource_crud_mariadb_template.go
@@ -20,117 +20,117 @@ func DataSourceCrudMariaDb(
 ) string {
 	tmpl := `package {name}
 	
-	import (
-		"fmt"
-		"{module}/{path}/database"
-		"{module}/internal/pkg/response"
-		"github.com/prongbang/sqlxwrapper/mrwrapper"
-	)
-	
-	type DataSource interface {
-		Count(params Params) int64
-		FindList(params Params) []{model}
-		FindById(id {pk}) {model}
-		Create(data *Create{model}) error
-		Update(data *Update{model}) error
-		Delete(id {pk}) error
+import (
+	"fmt"
+	"{module}/{path}/database"
+	"{module}/internal/pkg/response"
+	"github.com/prongbang/sqlxwrapper/mrwrapper"
+)
+
+type DataSource interface {
+	Count(params Params) int64
+	FindList(params Params) []{model}
+	FindById(id {pk}) {model}
+	Create(data *Create{model}) error
+	Update(data *Update{model}) error
+	Delete(id {pk}) error
+}
+
+type dataSource struct {
+	Driver database.Drivers
+}
+
+func (d *dataSource) Count(params Params) int64 {
+	conn := d.Driver.{driver}()
+	sql := "SELECT COUNT(id) FROM {table} WHERE 1=1 %s"
+	wheres := ""
+	args := []any{}
+
+	sql = fmt.Sprintf(sql, wheres)
+
+	return mrwrapper.Count(conn, sql, args...)
+}
+
+func (d *dataSource) FindList(params Params) []{model} {
+	conn := d.Driver.{driver}()
+	sql := "SELECT {columns} FROM {table} {alias} WHERE 1=1 %s ORDER BY {alias}.id"
+	wheres := ""
+	args := []any{}
+
+	if params.Limit > 0 && params.Offset >= 0 {
+		sql += " LIMIT ?"
+		args = append(args, params.Limit)
+		sql += " OFFSET ?"
+		args = append(args, params.Offset)
 	}
-	
-	type dataSource struct {
-		Driver database.Drivers
+
+	sql = fmt.Sprintf(sql, wheres)
+
+	return mrwrapper.SelectList[{model}](conn, sql, args...)
+}
+
+func (d *dataSource) FindById(id {pk}) {model} {
+	conn := d.Driver.{driver}()
+	sql := "SELECT {columns} FROM {table} {alias} WHERE {alias}.id = ?"
+
+	return mrwrapper.SelectOne[{model}](conn, sql, id)
+}
+
+func (d *dataSource) Create(data *Create{model}) error {
+	conn := d.Driver.{driver}()
+	sql := "INSERT INTO {table} ({fields}) VALUES ({questions})"
+	args := []any{
+		{values}
 	}
-	
-	func (d *dataSource) Count(params Params) int64 {
-		conn := d.Driver.{driver}()
-		sql := "SELECT COUNT(id) FROM {table} WHERE 1=1 %s"
-		wheres := ""
-		args := []any{}
-	
-		sql = fmt.Sprintf(sql, wheres)
-	
-		return mrwrapper.Count(conn, sql, args...)
-	}
-	
-	func (d *dataSource) FindList(params Params) []{model} {
-		conn := d.Driver.{driver}()
-		sql := "SELECT {columns} FROM {table} {alias} WHERE 1=1 %s ORDER BY {alias}.id"
-		wheres := ""
-		args := []any{}
-	
-		if params.Limit > 0 && params.Offset >= 0 {
-			sql += " LIMIT ?"
-			args = append(args, params.Limit)
-			sql += " OFFSET ?"
-			args = append(args, params.Offset)
+	tx, err := mrwrapper.Create(conn, sql, []any{&data.Id}, args...)
+	if err == nil {
+		if e := tx.Commit(); e != nil {
+			return response.NewCommitError()
 		}
-	
-		sql = fmt.Sprintf(sql, wheres)
-	
-		return mrwrapper.SelectList[{model}](conn, sql, args...)
+		return nil
 	}
-	
-	func (d *dataSource) FindById(id {pk}) {model} {
-		conn := d.Driver.{driver}()
-		sql := "SELECT {columns} FROM {table} {alias} WHERE {alias}.id = ?"
-	
-		return mrwrapper.SelectOne[{model}](conn, sql, id)
+	return response.NewInsertError()
+}
+
+func (d *dataSource) Update(data *Update{model}) error {
+	conn := d.Driver.{driver}()
+	params := map[string]interface{}{
+		"id": data.Id,
 	}
-	
-	func (d *dataSource) Create(data *Create{model}) error {
-		conn := d.Driver.{driver}()
-		sql := "INSERT INTO {table} ({fields}) VALUES ({questions})"
-		args := []any{
-			{values}
+	set := ""
+	sql := "UPDATE {table} SET %s WHERE id=:id"
+
+	{sets}
+
+	tx, err := mrwrapper.Update(conn, sql, set, params)
+	if err == nil {
+		if e := tx.Commit(); e != nil {
+			return response.NewCommitError()
 		}
-		tx, err := mrwrapper.Create(conn, sql, []any{&data.Id}, args...)
-		if err == nil {
-			if e := tx.Commit(); e != nil {
-				return response.NewCommitError()
-			}
-			return nil
-		}
-		return response.NewInsertError()
+		return nil
 	}
-	
-	func (d *dataSource) Update(data *Update{model}) error {
-		conn := d.Driver.{driver}()
-		params := map[string]interface{}{
-			"id": data.Id,
+	return response.NewUpdateError()
+}
+
+func (d *dataSource) Delete(id {pk}) error {
+	conn := d.Driver.{driver}()
+	sql := "DELETE FROM {table} WHERE id=?"
+
+	tx, err := mrwrapper.Delete(conn, sql, id)
+	if err == nil {
+		if e := tx.Commit(); e != nil {
+			return response.NewCommitError()
 		}
-		set := ""
-		sql := "UPDATE {table} SET %s WHERE id=:id"
-	
-		{sets}
-	
-		tx, err := mrwrapper.Update(conn, sql, set, params)
-		if err == nil {
-			if e := tx.Commit(); e != nil {
-				return response.NewCommitError()
-			}
-			return nil
-		}
-		return response.NewUpdateError()
+		return nil
 	}
-	
-	func (d *dataSource) Delete(id {pk}) error {
-		conn := d.Driver.{driver}()
-		sql := "DELETE FROM {table} WHERE id=?"
-	
-		tx, err := mrwrapper.Delete(conn, sql, id)
-		if err == nil {
-			if e := tx.Commit(); e != nil {
-				return response.NewCommitError()
-			}
-			return nil
-		}
-		return response.NewDeleteError()
+	return response.NewDeleteError()
+}
+
+func NewDataSource(driver database.Drivers) DataSource {
+	return &dataSource{
+		Driver: driver,
 	}
-	
-	func NewDataSource(driver database.Drivers) DataSource {
-		return &dataSource{
-			Driver: driver,
-		}
-	}`
+}`
 
 	tmpl = strings.ReplaceAll(tmpl, "{module}", module)
 	tmpl = strings.ReplaceAll(tmpl, "{path}", path)
